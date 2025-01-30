3f6eaa3d245a9261c1635314f21d70eee8b4ffcf
Marcos Moura <marcosfeitosamoura@gmail.com>
2025-01-30 17:46:17 -0300

    atualização api

---
+++ src/employee_api/Controllers/EmployeeController.cs
@@ -0,0 +1,77 @@
+using application.Interfaces;
+using domain.Entity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace employee_api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class EmployeeController : ControllerBase
+{
+    private readonly ILogger<EmployeeController> _logger;
+    private readonly ICreateEmployeeUseCase _createEmployeeUseCase;
+    private readonly IUpdateEmployeeUseCase _updateEmployeeUseCase;
+    private readonly IDeleteEmployeeUseCase _deleteEmployeeUseCase;
+    private readonly IQueryEmployeeUseCase _queryEmployeeUseCase;
+
+    public EmployeeController(ILogger<EmployeeController> logger,
+        ICreateEmployeeUseCase createEmployeeUseCase,
+        IUpdateEmployeeUseCase updateEmployeeUseCase,
+        IDeleteEmployeeUseCase deleteEmployeeUseCase,
+        IQueryEmployeeUseCase queryEmployeeUseCase)
+    {
+        _logger = logger;
+        _createEmployeeUseCase = createEmployeeUseCase;
+        _updateEmployeeUseCase = updateEmployeeUseCase;
+        _deleteEmployeeUseCase = deleteEmployeeUseCase;
+        _queryEmployeeUseCase = queryEmployeeUseCase;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] Employee employee)
+    {
+        var createResuErr = await _createEmployeeUseCase.Execute(employee);
+        
+        if(createResuErr != null)
+            return BadRequest(new { errors = createResuErr.ToList() });
+        
+        return Created();
+    }
+    
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Update(int id, [FromBody] Employee employee)
+    {
+        var updateResuErr = await _updateEmployeeUseCase.Execute(id, employee);
+        
+        if(updateResuErr != null)
+            return BadRequest(new { errors = updateResuErr.ToList() });
+        
+        return Created();
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> Get(int pagesize, int limit)
+    {
+        List<Employee> employees = await _queryEmployeeUseCase.Execute(pagesize, limit);
+        
+        if(employees == null)
+            return NoContent();
+        
+        return Ok(new
+        {
+            list = employees,
+            totalRecords = 11//Carregar no retorno
+        });
+    }
+    
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var deleteResuErr = await _deleteEmployeeUseCase.Execute(id);
+        
+        if(deleteResuErr != null)
+            return BadRequest(new { errors = deleteResuErr.ToList() });
+        
+        return Ok();
+    }
+}
---
+++ src/infra/Migrations/20250130202651_AtualizandoColunas.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class AtualizandoColunas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Employee",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Employee");
+        }
+    }
+}
