3d1096e9081cea92e9b0f8a75a6abf9c88ba3314
Marcos Moura <marcosfeitosamoura@gmail.com>
2025-02-01 22:28:01 -0300

    Add Migrations

---
+++ src/infra/Migrations/20250131200451_AtualizacaoNovaTabelaContatos.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class AtualizacaoNovaTabelaContatos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Phone",
+                table: "Employee");
+
+            migrationBuilder.CreateTable(
+                name: "EmployeeContact",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    EmployeeId = table.Column<int>(type: "int", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "varchar(20)", maxLength: 20, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    ContactName = table.Column<string>(type: "varchar(150)", maxLength: 150, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployeeContact", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EmployeeContact_Employee_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "Employee",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmployeeContact_EmployeeId",
+                table: "EmployeeContact",
+                column: "EmployeeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EmployeeContact");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Phone",
+                table: "Employee",
+                type: "varchar(15)",
+                maxLength: 15,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
---
+++ src/infra/Migrations/20250202010036_AtualizandoColunaNovoCampo.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class AtualizandoColunaNovoCampo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ManagerId",
+                table: "Employee",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ManagerId",
+                table: "Employee");
+        }
+    }
+}
